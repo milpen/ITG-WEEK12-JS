4e6e175d61aec78d957f830ab6cd065cf8f38842
milpen <milpen@mail.ru>
2023-05-30 23:54:19 +0500

    final

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+const input1=document.getElementById("name");
+const buttonSubmit=document.getElementById("btn");
+const input2=document.getElementById('link');
+const input3=document.getElementById('comment');
 
 function userName (str1) {
 let result1=str1.trim();
@@ -12,29 +15,24 @@ result1 = result1[0].toUpperCase()+result1.slice(1).toLowerCase();
 return result1;
 }
 //userName('YuTYYmRR');
-const input1=document.getElementById("name");
-const buttonSubmit=document.getElementById("btn");
-const input2=document.getElementById('link');
-const input3=document.getElementById('comment');
 
 function imageLink (src) {
   let parentImageLink =document.querySelector("#chat");
   let image=document.createElement('img');
-  image.src=src;
+  image.src=input2.value;
   image.alt="UserAvatar";
   parentImageLink.appendChild(image);
 }
 
 function checkSpam(str2) {
-  let result2= /viagra | xxx/ig;
+  let result2= /viagra|xxx/ig;
   return str2.replace(result2, ' *** ');
 }
 //console.log(checkSpam('buy VIAgra now'));
 //console.log(checkSpam('free xxx'));
 //console.log(checkSpam('innocent rabbit'));
-//let checkedComment = checkSpam(input3.value);
 
-buttonSubmit.addEventListener("click", commentField, outputUserName);
+buttonSubmit.addEventListener("click", commentField, outputUserName, outputUserText);
 
 function outputUserName (userName) {
   let parentUserName=document.querySelector("#chat");
@@ -44,22 +42,22 @@ function outputUserName (userName) {
   parentUserName.appendChild(outputName);
 }
 
+function outputUserText (checkSpam) {
+  let parentUserText=document.querySelector("#chat");
+  let outputText=document.createElement('p');
+  outputText.textContent=checkSpam(input3.value);
+  parentUserText.appendChild(outputText);
+}
+
 function commentField(evt) {
   evt.preventDefault();
-  const chatName = document.getElementById("chat");
+  /*const chatName = document.getElementById("chat");
   let checkedName=userName(input1.value);
   chatName.textContent = checkedName;
   const chatComment = document.getElementById("chat");
   let checkedComment = checkSpam(input3.value);
-  chatComment.textContent = checkedComment;
+  chatComment.textContent = checkedComment;*/
   outputUserName(userName);
   imageLink();
+  outputUserText(checkSpam);
 }
-
-/*buttonSubmit.addEventListener('click', (evt)=> {
-event.preventDefault();
-userName(input1.value);
-imageLink(input2.value);
-checkSpam(input3.value);
-
-});*/
